1697447d53924edea940ff2c4dc26a5e1503b9c2
arussm <kimjng10@naver.com>
2021-05-18 16:25:00 +0900

    5월18일

---
+++ 02_DI/src/main/java/com/yydh/myapp/AppleSpeaker.java
@@ -0,0 +1,19 @@
+package com.yydh.myapp;
+
+public class AppleSpeaker implements Speaker {
+	public AppleSpeaker() {
+		System.out.println("===> AppelSpeaker 객체 생성");
+	}
+
+	@Override
+	public void voluemeUp() {
+		System.out.println("AppelSpeaker---볼륨을 올린다.");
+		
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("AppelSpeaker---볼륨을 내린다.");
+		
+	}
+}
